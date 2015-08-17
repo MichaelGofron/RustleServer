7499005de27a37462b042176638cf6254fd05e2e
MichaelGofron <gofronm@yahoo.com>
2015-08-16 21:01:23 -0700

    Set up database use

---
+++ app/db/db.js
@@ -0,0 +1,5 @@
+var mysql = require('mysql');
+
+exports.runQuery = function(query, values, cb){
+  // establish connection then run a query
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,8 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/my', function(req,res,next){
-  res.render('index', { title: 'MyOwn'});
+router.post('/sendPhoto', function(req, res, next){
+  // must set up a post to the database
+  // must somehow link to the database where we can save some units in the db.
+  res.end("maybe photo");
 });
 
 module.exports = router;
