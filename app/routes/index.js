b0625041f30a4db698da599696107a3a3f04b251
MichaelGofron <gofronm@yahoo.com>
2015-08-17 11:39:57 -0700

    Add views & index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,12 @@ router.get('/', function(req, res, next) {
 router.post('/sendPhoto', function(req, res, next){
   // must set up a post to the database
   // must somehow link to the database where we can save some units in the db.
+  console.log("req === ",req.body);
+  db.runQuery("INSERT INTO comment (message) values ($1);", [req.body.photoText], function(result){
+    console.log("result ==== ", result);
+  });
   res.end("maybe photo");
+
 });
 
 module.exports = router;
