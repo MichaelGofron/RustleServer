9a8092429d6959c37ee68a7764ec7a4039ae5b7b
MichaelGofron <gofronm@yahoo.com>
2015-08-18 21:17:44 -0700

    Flesh out index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,4 +18,17 @@ router.post('/sendPhoto', function(req, res, next){
 
 });
 
+// must be able to upload a photo
+// when uploading a photo we to generate with s3 a storage place for our photo
+// get that url and then upload the link, the location, and any caption into the table
+// pass in UIImage, Caption, location
+router.post('/uploadPhoto', function(req,res,next){
+  console.log("req.body.keys === ", Object.keys(req.body));
+  console.log("req.body === ", req.body);
+  db.runQuery("INSERT INTO comment (message) values ($1);", ["Hello there"], function(result){
+    console.log("result ==== ", result);
+  });
+  res.end();
+});
+
 module.exports = router;
